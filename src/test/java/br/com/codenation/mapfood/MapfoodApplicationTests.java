f9c49976906e75435b15ca90360fa0b43ccf54f9
Wellington Felix <wfmf@cin.ufpe.br>
2019-02-13 21:49:05 -0300

    add initial project structure

---
+++ src/test/java/br/com/codenation/mapfood/MapfoodApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.codenation.mapfood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapfoodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
