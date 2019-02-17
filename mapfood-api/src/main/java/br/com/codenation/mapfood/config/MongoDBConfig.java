8827ae178be72cdfc1e345cb72668fb9e5f73fd2
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-02-17 18:14:57 -0300

    [Task] Add MongoDB Config

---
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/config/MongoDBConfig.java
@@ -0,0 +1,25 @@
+package br.com.codenation.mapfood.config;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoDBConfig extends AbstractMongoConfiguration {
+
+  @Override
+  public MongoClient mongoClient() {
+    String connection =
+          "mongodb://mapfood:mapfood1104@ds137255.mlab.com:37255/mapfood";
+
+    return new MongoClient(new MongoClientURI(connection));
+  }
+
+  @Override
+  protected String getDatabaseName() {
+    return "mapfood";
+  }
+}
