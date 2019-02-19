308b70bf88b00a94772a2e31a625a5fff225800e
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-02-18 23:43:11 -0300

    Aplicar mudança de tipo para repository e resource

--- mapfood-api/src/main/java/br/com/codenation/mapfood/repository/RestaurantsRepository.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/repository/RestaurantsRepository.java
@@ -1,11 +1,12 @@
 package br.com.codenation.mapfood.repository;
 
 import br.com.codenation.mapfood.document.Restaurant;
+import org.bson.types.ObjectId;
 import org.springframework.data.geo.Circle;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface RestaurantsRepository extends MongoRepository<Restaurant, String> {
+public interface RestaurantsRepository extends MongoRepository<Restaurant, ObjectId> {
     List<Restaurant> findByLocationWithin(Circle c);
 }
