26a293a8085cbfc472a1882127e1e8940938370f
cleberson <clebrsonn@hotmail.com>
2019-02-23 17:14:17 -0300

    delivery

--- mapfood-api/src/main/java/br/com/codenation/mapfood/document/Motorcyclist.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/document/Motorcyclist.java
@@ -1,11 +1,11 @@
 package br.com.codenation.mapfood.document;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @Document(collection = "deliverymen")
@@ -13,6 +13,8 @@ public class Motorcyclist {
 
     @Id
     private String _id;
+
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
     private GeoJsonPoint location;
 
     private String original_id;
--- mapfood-api/src/main/java/br/com/codenation/mapfood/service/RestaurantService.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/service/RestaurantService.java
@@ -1,10 +1,8 @@
 package br.com.codenation.mapfood.service;
 
-import br.com.codenation.mapfood.document.DeliveryMan;
 import br.com.codenation.mapfood.document.Restaurant;
-import br.com.codenation.mapfood.repository.DeliveryManRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Circle;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +10,4 @@ import java.util.List;
 
 @Service
 public class RestaurantService {
-
-    @Autowired
-    private DeliveryManRepository deliveryManRepository;
-
-    public List<DeliveryMan> searchDeliveryMan(Restaurant restaurant){
-        final GeoJsonPoint location = restaurant.getLocation();
-
-
-        return deliveryManRepository.findByLocationNear(location, new Distance(5));
-    }
 }
