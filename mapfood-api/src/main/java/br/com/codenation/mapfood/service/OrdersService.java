33bd579dcee3d0f9123663c8d0796c9418e38774
Wellington Felix <wfmf@cin.ufpe.br>
2019-02-24 14:37:42 -0300

    refactor OrdersService to implement an interface

--- mapfood-api/src/main/java/br/com/codenation/mapfood/service/OrdersService.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/service/OrdersService.java
@@ -1,70 +1,14 @@
 package br.com.codenation.mapfood.service;
 
-import br.com.codenation.mapfood.document.Item;
 import br.com.codenation.mapfood.document.Order;
-import br.com.codenation.mapfood.document.Restaurant;
-import br.com.codenation.mapfood.exception.InvalidOrderException;
-import br.com.codenation.mapfood.exception.OrderNotFoundException;
-import br.com.codenation.mapfood.exception.RestaurantNotFoundException;
-import br.com.codenation.mapfood.repository.OrdersRepository;
-import br.com.codenation.mapfood.repository.RestaurantsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static br.com.codenation.mapfood.document.OrderStatus.PENDING;
+public interface OrdersService {
 
-@Service
-public class OrdersService {
-    private final OrdersRepository ordersRepository;
-    private final RestaurantsRepository restaurantsRepository;
+    Order save(Order order);
 
-    @Autowired
-    public OrdersService(OrdersRepository ordersRepository, RestaurantsRepository restaurantsRepository) {
-        this.ordersRepository = ordersRepository;
-        this.restaurantsRepository = restaurantsRepository;
-    }
+    List<Order> getAll();
 
-    public Order save(Order order) throws InvalidOrderException {
-        String restaurantId = order.getRestaurant().getId();
-
-        Optional<Restaurant> restaurant = restaurantsRepository.findById(restaurantId);
-        boolean isValidOrder;
-
-        if (restaurant.isPresent()) {
-            List<String> incomingItemIds = order.getItems()
-                    .stream()
-                    .map(item -> item.getItem().getId())
-                    .collect(Collectors.toList());
-
-            List<String> restaurantItemIds = restaurant.get().getItems()
-                    .stream()
-                    .map(Item::getId)
-                    .collect(Collectors.toList());
-
-            isValidOrder = restaurantItemIds.containsAll(incomingItemIds);
-        } else {
-            throw new RestaurantNotFoundException();
-        }
-
-        if (isValidOrder) {
-            order.setStatus(PENDING);
-            order.setTimestamp(LocalDateTime.now());
-            return ordersRepository.save(order);
-        }
-
-        throw new InvalidOrderException("Só é permitido pedidos de um resturante por vez!");
-    }
-
-    public List<Order> getAll() {
-        return ordersRepository.findAll();
-}
-
-    public Order getOne(String id) {
-         return ordersRepository.findById(id).orElseThrow(OrderNotFoundException::new);
-    }
+    Order getOne(String id);
 }
---
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/service/impl/OrdersServiceImpl.java
@@ -0,0 +1,74 @@
+package br.com.codenation.mapfood.service.impl;
+
+import br.com.codenation.mapfood.document.Item;
+import br.com.codenation.mapfood.document.Order;
+import br.com.codenation.mapfood.document.Restaurant;
+import br.com.codenation.mapfood.exception.InvalidOrderException;
+import br.com.codenation.mapfood.exception.OrderNotFoundException;
+import br.com.codenation.mapfood.exception.RestaurantNotFoundException;
+import br.com.codenation.mapfood.repository.OrdersRepository;
+import br.com.codenation.mapfood.repository.RestaurantsRepository;
+import br.com.codenation.mapfood.service.OrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static br.com.codenation.mapfood.document.OrderStatus.PENDING;
+
+@Service
+public class OrdersServiceImpl implements OrdersService  {
+    private final OrdersRepository ordersRepository;
+    private final RestaurantsRepository restaurantsRepository;
+
+    @Autowired
+    public OrdersServiceImpl(OrdersRepository ordersRepository, RestaurantsRepository restaurantsRepository) {
+        this.ordersRepository = ordersRepository;
+        this.restaurantsRepository = restaurantsRepository;
+    }
+
+    @Override
+    public Order save(Order order) {
+        String restaurantId = order.getRestaurant().getId();
+
+        Optional<Restaurant> restaurant = restaurantsRepository.findById(restaurantId);
+        boolean isValidOrder;
+
+        if (restaurant.isPresent()) {
+            List<String> incomingItemIds = order.getItems()
+                    .stream()
+                    .map(item -> item.getItem().getId())
+                    .collect(Collectors.toList());
+
+            List<String> restaurantItemIds = restaurant.get().getItems()
+                    .stream()
+                    .map(Item::getId)
+                    .collect(Collectors.toList());
+
+            isValidOrder = restaurantItemIds.containsAll(incomingItemIds);
+        } else {
+            throw new RestaurantNotFoundException();
+        }
+
+        if (isValidOrder) {
+            order.setStatus(PENDING);
+            order.setTimestamp(LocalDateTime.now());
+            return ordersRepository.save(order);
+        }
+
+        throw new InvalidOrderException("Só é permitido pedidos de um resturante por vez!");
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return ordersRepository.findAll();
+    }
+
+    @Override
+    public Order getOne(String id) {
+        return ordersRepository.findById(id).orElseThrow(OrderNotFoundException::new);
+    }
+}
