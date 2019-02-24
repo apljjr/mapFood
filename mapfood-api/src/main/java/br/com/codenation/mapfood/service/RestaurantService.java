c8266339346f4320e5d54e70b922c236cab9fac4
Adeilson Pereira <aplj.adeilson@gmail.com>
2019-02-24 20:53:59 -0300

    Camada service - RestaurantService

--- mapfood-api/src/main/java/br/com/codenation/mapfood/service/RestaurantService.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/service/RestaurantService.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class RestaurantService {
+public interface RestaurantService {
 }
---
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/service/impl/RestaurantServiceImpl.java
@@ -0,0 +1,6 @@
+package br.com.codenation.mapfood.service.impl;
+
+import br.com.codenation.mapfood.service.RestaurantService;
+
+public class RestaurantServiceImpl implements RestaurantService {
+}
