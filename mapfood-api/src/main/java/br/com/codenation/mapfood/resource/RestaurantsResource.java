fd5cb0ba6790f4b82c8faed076b57b83d7de7951
Adeilson Pereira <aplj.adeilson@gmail.com>
2019-02-24 22:03:29 -0300

    Report

--- mapfood-api/src/main/java/br/com/codenation/mapfood/resource/RestaurantsResource.java
+++ mapfood-api/src/main/java/br/com/codenation/mapfood/resource/RestaurantsResource.java
@@ -4,18 +4,17 @@ import br.com.codenation.mapfood.document.Restaurant;
 import br.com.codenation.mapfood.service.RestaurantService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Api (value = "Restaurant", description = "Manipulating Restaurant")
@@ -47,5 +46,11 @@ public class RestaurantsResource {
     @GetMapping (value = "/{id}", produces = "application/json")
     public ResponseEntity<?> findRestaurantById(@PathVariable String id) {
         return new ResponseEntity<>(restaurantService.findById(id), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Generating order reports")
+    @GetMapping (value = "/report/{id}")
+    public void generateRepost (HttpServletResponse response) throws JRException, IOException {
+
     }
 }
