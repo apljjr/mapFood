f6df42851d5aa0e9d0cd8f0811f216bd1ab9c2e2
Wellington Felix <wfmf@cin.ufpe.br>
2019-02-26 01:29:31 -0300

    remove unused classes

--- mapfood-api/src/main/java/br/com/codenation/mapfood/service/RouteService.java
+++
@@ -1,9 +0,0 @@
-package br.com.codenation.mapfood.service;
-
-import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
-
-import java.util.List;
-
-public interface RouteService {
-    String callService(GeoJsonPoint motorclycleLocation, GeoJsonPoint userLocation, List<GeoJsonPoint> restautantLocation);
-}
